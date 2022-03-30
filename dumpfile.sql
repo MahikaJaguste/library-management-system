2ab1d640cb1a8b491f3daebfd036a27e59a22068
ShrreyaSingh <shrreya7singh@gmail.com>
2022-03-30 19:28:25 +0530

    added dumpfile

--- dumpfile.sql
+++ dumpfile.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for macos12.0 (arm64)
 --
--- Host: localhost    Database: assignment_6
+-- Host: localhost    Database: a6_library
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -35,6 +35,7 @@ CREATE TABLE `Authors` (
 
 LOCK TABLES `Authors` WRITE;
 /*!40000 ALTER TABLE `Authors` DISABLE KEYS */;
+INSERT INTO `Authors` VALUES (123,'Ping'),(234,'Sir Arthur Conan Doyle'),(345,'HP & Kennesy'),(428,'Lola'),(897,'Kiman');
 /*!40000 ALTER TABLE `Authors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,6 +62,7 @@ CREATE TABLE `Book_Auth` (
 
 LOCK TABLES `Book_Auth` WRITE;
 /*!40000 ALTER TABLE `Book_Auth` DISABLE KEYS */;
+INSERT INTO `Book_Auth` VALUES (12345,123),(34567,123),(23456,234),(45678,345),(56789,428),(67890,897);
 /*!40000 ALTER TABLE `Book_Auth` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -87,6 +89,7 @@ CREATE TABLE `Book_Genre` (
 
 LOCK TABLES `Book_Genre` WRITE;
 /*!40000 ALTER TABLE `Book_Genre` DISABLE KEYS */;
+INSERT INTO `Book_Genre` VALUES (12345,145),(12345,345),(67890,589),(12345,678),(34567,678),(56789,738),(23456,890),(45678,890);
 /*!40000 ALTER TABLE `Book_Genre` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -113,6 +116,7 @@ CREATE TABLE `Book_Issue` (
 
 LOCK TABLES `Book_Issue` WRITE;
 /*!40000 ALTER TABLE `Book_Issue` DISABLE KEYS */;
+INSERT INTO `Book_Issue` VALUES (4238,12345),(4898,23456),(4278,34567),(4232,45678);
 /*!40000 ALTER TABLE `Book_Issue` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,6 +144,7 @@ CREATE TABLE `Book_Location` (
 
 LOCK TABLES `Book_Location` WRITE;
 /*!40000 ALTER TABLE `Book_Location` DISABLE KEYS */;
+INSERT INTO `Book_Location` VALUES (12345,'34709','A'),(56789,'34709','A'),(67890,'34709','A'),(23456,'34709','B'),(34567,'98717','A'),(45678,'98717','B');
 /*!40000 ALTER TABLE `Book_Location` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,6 +171,7 @@ CREATE TABLE `Book_Pub` (
 
 LOCK TABLES `Book_Pub` WRITE;
 /*!40000 ALTER TABLE `Book_Pub` DISABLE KEYS */;
+INSERT INTO `Book_Pub` VALUES (34567,34),(56789,34),(45678,56),(67890,56),(12345,98),(23456,98);
 /*!40000 ALTER TABLE `Book_Pub` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,31 +199,33 @@ CREATE TABLE `Books` (
 
 LOCK TABLES `Books` WRITE;
 /*!40000 ALTER TABLE `Books` DISABLE KEYS */;
+INSERT INTO `Books` VALUES (12345,'Science Marvels',560,'True'),(23456,'Archaeology',245,'False'),(34567,'Astro Physics',500,'True'),(45678,'Panchatantra',200,'True'),(56789,'Computer Organisation and Architecture',1200,'True'),(67890,'Sherlock Holmes',820,'True');
 /*!40000 ALTER TABLE `Books` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Books_Purchase`
+-- Table structure for table `books_purchase`
 --
 
-DROP TABLE IF EXISTS `Books_Purchase`;
+DROP TABLE IF EXISTS `books_purchase`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Books_Purchase` (
+CREATE TABLE `books_purchase` (
   `purchase_ID` int NOT NULL,
   `price` int NOT NULL,
-  `purchase_date` datetime DEFAULT NULL,
+  `purchase_date` date DEFAULT NULL,
   PRIMARY KEY (`purchase_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Books_Purchase`
+-- Dumping data for table `books_purchase`
 --
 
-LOCK TABLES `Books_Purchase` WRITE;
-/*!40000 ALTER TABLE `Books_Purchase` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Books_Purchase` ENABLE KEYS */;
+LOCK TABLES `books_purchase` WRITE;
+/*!40000 ALTER TABLE `books_purchase` DISABLE KEYS */;
+INSERT INTO `books_purchase` VALUES (1,1200,'2012-06-27'),(2,1050,'2012-07-27'),(3,200,'2014-07-12'),(4,3560,'2012-06-27');
+/*!40000 ALTER TABLE `books_purchase` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -242,6 +250,7 @@ CREATE TABLE `Faculty` (
 
 LOCK TABLES `Faculty` WRITE;
 /*!40000 ALTER TABLE `Faculty` DISABLE KEYS */;
+INSERT INTO `Faculty` VALUES (200,'HSS',79000),(301,'CSE',135000),(302,'Mechanical',85000),(811,'Palaeontology',90000);
 /*!40000 ALTER TABLE `Faculty` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,6 +275,7 @@ CREATE TABLE `Genres` (
 
 LOCK TABLES `Genres` WRITE;
 /*!40000 ALTER TABLE `Genres` DISABLE KEYS */;
+INSERT INTO `Genres` VALUES (145,'Course Book'),(345,'Crime'),(589,'Fiction'),(678,'History'),(738,'Intriguing'),(890,'Non-fiction');
 /*!40000 ALTER TABLE `Genres` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -289,6 +299,7 @@ CREATE TABLE `Job_Salary` (
 
 LOCK TABLES `Job_Salary` WRITE;
 /*!40000 ALTER TABLE `Job_Salary` DISABLE KEYS */;
+INSERT INTO `Job_Salary` VALUES ('Caretaker',56000),('Finance Accountant',60000),('Hostel Manager',70000),('Library Entry Superviser',35000),('Library Manager',80000),('Library Worker',75000),('Technician',65000),('Watchman',56000);
 /*!40000 ALTER TABLE `Job_Salary` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -313,6 +324,7 @@ CREATE TABLE `Library_Staff` (
 
 LOCK TABLES `Library_Staff` WRITE;
 /*!40000 ALTER TABLE `Library_Staff` DISABLE KEYS */;
+INSERT INTO `Library_Staff` VALUES (561,'Manager'),(602,'Books sequence manager'),(603,'Entry Superviser'),(604,'Front-end technician'),(123,'General incharge');
 /*!40000 ALTER TABLE `Library_Staff` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -339,6 +351,7 @@ CREATE TABLE `Library_Staff_Working_Hours` (
 
 LOCK TABLES `Library_Staff_Working_Hours` WRITE;
 /*!40000 ALTER TABLE `Library_Staff_Working_Hours` DISABLE KEYS */;
+INSERT INTO `Library_Staff_Working_Hours` VALUES (123,'15:10:10','20:10:10'),(561,'15:10:10','20:10:10'),(602,'09:10:10','15:10:10'),(603,'07:10:10','15:10:10'),(604,'07:10:10','15:10:10');
 /*!40000 ALTER TABLE `Library_Staff_Working_Hours` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -362,6 +375,7 @@ CREATE TABLE `Library_Systems` (
 
 LOCK TABLES `Library_Systems` WRITE;
 /*!40000 ALTER TABLE `Library_Systems` DISABLE KEYS */;
+INSERT INTO `Library_Systems` VALUES (1011,'OS: Windows, NVIDIA GPU'),(1022,'OS: LINUX, No GPU'),(1023,'OS: Windows, NO GPU'),(1024,'OS: MacOS, TPU'),(1035,'OS: Windows, NVIDIA GPU');
 /*!40000 ALTER TABLE `Library_Systems` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -387,6 +401,7 @@ CREATE TABLE `Location` (
 
 LOCK TABLES `Location` WRITE;
 /*!40000 ALTER TABLE `Location` DISABLE KEYS */;
+INSERT INTO `Location` VALUES ('34709','A',780),('34709','B',780),('98717','A',185),('98717','B',184);
 /*!40000 ALTER TABLE `Location` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -412,6 +427,7 @@ CREATE TABLE `Other_Staff` (
 
 LOCK TABLES `Other_Staff` WRITE;
 /*!40000 ALTER TABLE `Other_Staff` DISABLE KEYS */;
+INSERT INTO `Other_Staff` VALUES (401,5),(723,6),(721,4),(601,4),(605,5);
 /*!40000 ALTER TABLE `Other_Staff` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -435,6 +451,7 @@ CREATE TABLE `Penalties` (
 
 LOCK TABLES `Penalties` WRITE;
 /*!40000 ALTER TABLE `Penalties` DISABLE KEYS */;
+INSERT INTO `Penalties` VALUES (1084,'Returned Late'),(4398,'Returned Late'),(7920,'Returned Late'),(8734,'Returned Late');
 /*!40000 ALTER TABLE `Penalties` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -465,6 +482,7 @@ CREATE TABLE `Publishers` (
 
 LOCK TABLES `Publishers` WRITE;
 /*!40000 ALTER TABLE `Publishers` DISABLE KEYS */;
+INSERT INTO `Publishers` VALUES (34,'Cinemix','789','Gandhinagar road','Gandhinagar','Gujarat','382355','cinemix00@gmail.com'),(56,'McGrawHill','454','Nehru road','Mumbai','Maharashtra','400180','nehru_publishers@gmail.com'),(98,'Penguin','234','LBS road','Mumbai','Maharashtra','400080','penguin7k@gmail.com');
 /*!40000 ALTER TABLE `Publishers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -477,7 +495,7 @@ DROP TABLE IF EXISTS `Publishers_Phone`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Publishers_Phone` (
   `publisher_ID` int NOT NULL,
-  `phone_number` int NOT NULL,
+  `phone_number` varchar(45) NOT NULL,
   PRIMARY KEY (`publisher_ID`,`phone_number`),
   CONSTRAINT `publisher_ID_phone` FOREIGN KEY (`publisher_ID`) REFERENCES `Publishers` (`publisher_ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -489,6 +507,7 @@ CREATE TABLE `Publishers_Phone` (
 
 LOCK TABLES `Publishers_Phone` WRITE;
 /*!40000 ALTER TABLE `Publishers_Phone` DISABLE KEYS */;
+INSERT INTO `Publishers_Phone` VALUES (34,'8911747017'),(56,'9172124701'),(98,'9242834791'),(98,'9782792345');
 /*!40000 ALTER TABLE `Publishers_Phone` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -515,6 +534,7 @@ CREATE TABLE `Purchase` (
 
 LOCK TABLES `Purchase` WRITE;
 /*!40000 ALTER TABLE `Purchase` DISABLE KEYS */;
+INSERT INTO `Purchase` VALUES (12345,1),(23456,2),(34567,2),(45678,3),(56789,4),(67890,4);
 /*!40000 ALTER TABLE `Purchase` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -539,6 +559,7 @@ CREATE TABLE `Staff` (
 
 LOCK TABLES `Staff` WRITE;
 /*!40000 ALTER TABLE `Staff` DISABLE KEYS */;
+INSERT INTO `Staff` VALUES (123,'Library Worker'),(401,'Caretaker'),(561,'Library Manager'),(601,'Technician'),(602,'Library Worker'),(603,'Library Entry Superviser'),(604,'Library Worker'),(605,'Watchman'),(721,'Finance Accountant'),(723,'Hostel Manager');
 /*!40000 ALTER TABLE `Staff` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -570,6 +591,7 @@ CREATE TABLE `Strike` (
 
 LOCK TABLES `Strike` WRITE;
 /*!40000 ALTER TABLE `Strike` DISABLE KEYS */;
+INSERT INTO `Strike` VALUES (1084,4278,5,500),(4398,4232,3,300),(7920,4230,2,200),(8734,4238,4,400);
 /*!40000 ALTER TABLE `Strike` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -594,6 +616,7 @@ CREATE TABLE `Student` (
 
 LOCK TABLES `Student` WRITE;
 /*!40000 ALTER TABLE `Student` DISABLE KEYS */;
+INSERT INTO `Student` VALUES (101,'BTECH'),(102,'MTECH'),(103,'PHD'),(922,'PHD');
 /*!40000 ALTER TABLE `Student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -620,6 +643,7 @@ CREATE TABLE `System_Allocation` (
 
 LOCK TABLES `System_Allocation` WRITE;
 /*!40000 ALTER TABLE `System_Allocation` DISABLE KEYS */;
+INSERT INTO `System_Allocation` VALUES (561,1011),(602,1022),(603,1023),(604,1024),(123,1035);
 /*!40000 ALTER TABLE `System_Allocation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -647,6 +671,7 @@ CREATE TABLE `Transaction` (
 
 LOCK TABLES `Transaction` WRITE;
 /*!40000 ALTER TABLE `Transaction` DISABLE KEYS */;
+INSERT INTO `Transaction` VALUES (4211,'2021-07-15 00:00:00','2021-07-24 00:00:00','2021-07-20 00:00:00'),(4230,'2021-07-14 00:00:00','2021-07-20 00:00:00','2021-07-22 00:00:00'),(4232,'2021-07-12 00:00:00','2021-07-20 00:00:00','2021-07-23 00:00:00'),(4238,'2021-07-12 00:00:00','2021-07-20 00:00:00','2021-07-24 00:00:00'),(4278,'2021-03-10 00:00:00','2022-07-15 00:00:00','2022-07-20 00:00:00'),(4473,'2021-07-11 00:00:00','2021-07-24 00:00:00','2021-07-20 00:00:00'),(4898,'2022-03-03 00:00:00','2022-04-05 00:00:00',NULL);
 /*!40000 ALTER TABLE `Transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -673,6 +698,7 @@ CREATE TABLE `User_Issue` (
 
 LOCK TABLES `User_Issue` WRITE;
 /*!40000 ALTER TABLE `User_Issue` DISABLE KEYS */;
+INSERT INTO `User_Issue` VALUES (4238,101),(4232,102),(4278,103),(4898,200);
 /*!40000 ALTER TABLE `User_Issue` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -696,6 +722,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (101,'Kim'),(102,'Bob'),(103,'Siya'),(123,'Snape'),(200,'Rachel'),(301,'Sam'),(302,'Sam'),(401,'Gita'),(561,'Monica'),(601,'Harry'),(602,'Ginny'),(603,'Hermoine'),(604,'Ron'),(605,'Neville'),(721,'Chandler'),(723,'Lily'),(811,'Ross'),(922,'Phoebe');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -708,7 +735,7 @@ DROP TABLE IF EXISTS `Users_Phone`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Users_Phone` (
   `user_ID` int NOT NULL,
-  `phone_number` int NOT NULL,
+  `phone_number` varchar(45) NOT NULL,
   PRIMARY KEY (`user_ID`,`phone_number`),
   CONSTRAINT `user_ID` FOREIGN KEY (`user_ID`) REFERENCES `Users` (`user_ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -720,12 +747,9 @@ CREATE TABLE `Users_Phone` (
 
 LOCK TABLES `Users_Phone` WRITE;
 /*!40000 ALTER TABLE `Users_Phone` DISABLE KEYS */;
+INSERT INTO `Users_Phone` VALUES (101,'1234567890'),(101,'1345678901'),(102,'3456789012'),(103,'4567890123'),(200,'7890123456'),(301,'5678901234'),(302,'6789012345');
 /*!40000 ALTER TABLE `Users_Phone` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'assignment_6'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -736,4 +760,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-27 17:21:50
+-- Dump completed on 2022-03-27 19:41:17
